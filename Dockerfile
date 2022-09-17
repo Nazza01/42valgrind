b696897693fd74fb17c5692556d8cd76f3e369ff
Nazza01 <nate_thomas_95@hotmail.com>
2022-09-17 23:12:07 +0930

    Adding mulitple tests for leak checkers

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 # syntax = docker/dockerfile:1.2
 
 FROM gcc:latest
-LABEL version="4.0"
+LABEL version="5.0"
 LABEL author="Nathanael"
 
 RUN apt-get update && apt-get install -y \
+	vim \
 	valgrind \
 	xorg \
 	libxext-dev \
-	zlib1g-dev \
 	libbsd-dev
 
 RUN mkdir /code
--- ExampleProject/testLeak/src/main.c
+++ ExampleProject/testLeak/src/main.c
@@ -5,25 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: Nathanael <nervin@student.42adel.org.au    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/14 13:09:44 by Nathanael         #+#    #+#             */
-/*   Updated: 2022/09/14 14:18:24 by Nathanael        ###   ########.fr       */
+/*   Created: 2022/09/17 22:20:14 by Nathanael         #+#    #+#             */
+/*   Updated: 2022/09/17 22:21:33 by Nathanael        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int	main(void)
+int	main(int argc, char const *argv[])
 {
-	void	*mlx;
-	void	*win;
-
-	mlx = mlx_init();
-	if (mlx != 0)
-		printf("Cannot init mlx");
-	win = mlx_new_window(mlx, 200, 200, "Hello World");
-	mlx_loop(mlx);
-
-	(void)win;
+	printf("Assigning size for an integer\n");
+	int *mal = malloc(sizeof(int));
+	
+	printf("Exiting Program\n");
 	return (0);
 }
